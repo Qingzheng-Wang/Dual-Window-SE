5cbf0604b7c510912d7c472e81aa796ef072b9d6
Qingzheng Wang <QingzhengWang@whu.edu.cn>
2023-07-03 22:12:49 +0800

    Add model
    complete implement model and test

---
+++ model/Blocks.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+# ==================================================
+# File Name:        Blocks.py
+# Author:           Qingzheng WANG
+# Time:             2023/7/3 14:02
+# Description:      Blocks in DNN, including ResBlock,
+#                   Deconv2D+PReLU+BN, Conv2D+PReLU+BN,
+#                   Conv2D+cauLN, Deconv2D
+# Function List:    
+# ===================================================
+
+import torch
+
+class ResBlock(torch.nn.Module):
+    def __init__(self, iFM, oFM, T, F, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iFM = iFM # input Feature Maps
+        self.oFM = oFM # output Feature Maps
+        self.T = T # time steps
+        self.F = F # frequency bins
+
+        self.depth_conv1 = torch.nn.Conv2d(in_channels=self.iFM, out_channels=self.oFM, kernel_size=(3, 3),
+                                           stride=(1, 1), padding=(1, 1), dilation=(1, 1),
+                                           groups=self.iFM)
+        self.point_conv1 = torch.nn.Conv2d(in_channels=self.iFM, out_channels=self.oFM, kernel_size=(1, 1))
+        self.prelu1 = torch.nn.PReLU()
+        self.bn1 = torch.nn.BatchNorm2d(num_features=self.oFM)
+        self.ds_conv1 = torch.nn.Sequential(self.depth_conv1, self.point_conv1, self.prelu1, self.bn1)  # depthwise separable convolution
+
+        self.depth_conv2 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(3, 3),
+                                           stride=(1, 1), padding=(2, 1), dilation=(2, 1),
+                                           groups=self.oFM)
+        self.point_conv2 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(1, 1))
+        self.prelu2 = torch.nn.PReLU()
+        self.bn2 = torch.nn.BatchNorm2d(num_features=self.oFM)
+        self.ds_conv2 = torch.nn.Sequential(self.depth_conv2, self.point_conv2, self.prelu2, self.bn2)
+
+        self.depth_conv3 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(3, 3),
+                                           stride=(1, 1), padding=(4, 1), dilation=(4, 1),
+                                           groups=self.oFM)
+        self.point_conv3 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(1, 1))
+        self.prelu3 = torch.nn.PReLU()
+        self.bn3 = torch.nn.BatchNorm2d(num_features=self.oFM)
+        self.ds_conv3 = torch.nn.Sequential(self.depth_conv3, self.point_conv3, self.prelu3, self.bn3)
+
+        self.depth_conv4 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(3, 3),
+                                           stride=(1, 1), padding=(8, 1), dilation=(8, 1),
+                                           groups=self.oFM)
+        self.point_conv4 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(1, 1))
+        self.prelu4 = torch.nn.PReLU()
+        self.bn4 = torch.nn.BatchNorm2d(num_features=self.oFM)
+        self.ds_conv4 = torch.nn.Sequential(self.depth_conv4, self.point_conv4, self.prelu4, self.bn4)
+
+        self.depth_conv5 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(3, 3),
+                                           stride=(1, 1), padding=(16, 1), dilation=(16, 1),
+                                           groups=self.oFM)
+        self.point_conv5 = torch.nn.Conv2d(in_channels=self.oFM, out_channels=self.oFM, kernel_size=(1, 1))
+        self.prelu5 = torch.nn.PReLU()
+        self.bn5 = torch.nn.BatchNorm2d(num_features=self.oFM)
+        self.ds_conv5 = torch.nn.Sequential(self.depth_conv5, self.point_conv5, self.prelu5, self.bn5)
+
+    def forward(self, x):
+        x += self.ds_conv1(x)
+        x += self.ds_conv2(x)
+        x += self.ds_conv3(x)
+        x += self.ds_conv4(x)
+        x += self.ds_conv5(x)
+        return x
+
+class Deconv2DPReLUBN(torch.nn.Module):
+    def __init__(self, iFM, oFM, T, F, kernel_time, kernel_freq, stride_time, stride_freq, pad_time, pad_freq,
+                 dilation_time, dilation_freq, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iFM = iFM  # input Feature Maps
+        self.oFM = oFM  # output Feature Maps
+        self.T = T  # time steps
+        self.F = F  # frequency bins
+        self.kernel_time = kernel_time
+        self.kernel_freq = kernel_freq
+        self.stride_time = stride_time
+        self.stride_freq = stride_freq
+        self.pad_time = pad_time
+        self.pad_freq = pad_freq
+        self.dilation_time = dilation_time
+        self.dilation_freq = dilation_freq
+
+        self.deconv = torch.nn.ConvTranspose2d(in_channels=self.iFM, out_channels=self.oFM,
+                                               kernel_size=(self.kernel_time, self.kernel_freq),
+                                               stride=(self.stride_time, self.stride_freq),
+                                               padding=(self.pad_time, self.pad_freq),
+                                               dilation=(self.dilation_time, self.dilation_freq))
+        self.prelu = torch.nn.PReLU()
+        self.bn = torch.nn.BatchNorm2d(num_features=self.oFM)
+
+    def forward(self, x):
+        x = self.deconv(x)
+        x = self.prelu(x)
+        x = self.bn(x)
+        return x
+
+class Conv2DPReLUBN(torch.nn.Module):
+    def __init__(self, iFM, oFM, T, F, kernel_time, kernel_freq, stride_time, stride_freq, pad_time, pad_freq,
+                 dilation_time, dilation_freq, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iFM = iFM  # input Feature Maps
+        self.oFM = oFM  # output Feature Maps
+        self.T = T  # time steps
+        self.F = F  # frequency bins
+        self.kernel_time = kernel_time
+        self.kernel_freq = kernel_freq
+        self.stride_time = stride_time
+        self.stride_freq = stride_freq
+        self.pad_time = pad_time
+        self.pad_freq = pad_freq
+        self.dilation_time = dilation_time
+        self.dilation_freq = dilation_freq
+
+        self.conv = torch.nn.Conv2d(in_channels=self.iFM, out_channels=self.oFM,
+                                    kernel_size=(self.kernel_time, self.kernel_freq),
+                                    stride=(self.stride_time, self.stride_freq),
+                                    padding=(self.pad_time, self.pad_freq),
+                                    dilation=(self.dilation_time, self.dilation_freq))
+        self.prelu = torch.nn.PReLU()
+        self.bn = torch.nn.BatchNorm2d(num_features=self.oFM)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.prelu(x)
+        x = self.bn(x)
+        return x
+
+class Conv2DCauLN(torch.nn.Module):
+    def __init__(self, iFM, oFM, T, F, kernel_time, kernel_freq, stride_time, stride_freq, pad_time, pad_freq,
+                 dilation_time, dilation_freq, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iFM = iFM  # input Feature Maps
+        self.oFM = oFM  # output Feature Maps
+        self.T = T  # time steps
+        self.F = F  # frequency bins
+        self.kernel_time = kernel_time
+        self.kernel_freq = kernel_freq
+        self.stride_time = stride_time
+        self.stride_freq = stride_freq
+        self.pad_time = pad_time
+        self.pad_freq = pad_freq
+        self.dilation_time = dilation_time
+        self.dilation_freq = dilation_freq
+
+        self.conv = torch.nn.Conv2d(in_channels=self.iFM, out_channels=self.oFM,
+                                    kernel_size=(self.kernel_time, self.kernel_freq),
+                                    stride=(self.stride_time, self.stride_freq),
+                                    padding=(self.pad_time, self.pad_freq),
+                                    dilation=(self.dilation_time, self.dilation_freq))
+        self.ln = torch.nn.LayerNorm(normalized_shape=[self.oFM, self.T, self.F])
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.ln(x)
+        return x
+
+class Deconv2D(torch.nn.Module):
+    def __init__(self, iFM, oFM, T, F, kernel_time, kernel_freq, stride_time, stride_freq, pad_time, pad_freq,
+                 dilation_time, dilation_freq, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iFM = iFM  # input Feature Maps
+        self.oFM = oFM  # output Feature Maps
+        self.T = T  # time steps
+        self.F = F  # frequency bins
+        self.kernel_time = kernel_time
+        self.kernel_freq = kernel_freq
+        self.stride_time = stride_time
+        self.stride_freq = stride_freq
+        self.pad_time = pad_time
+        self.pad_freq = pad_freq
+        self.dilation_time = dilation_time
+        self.dilation_freq = dilation_freq
+
+        self.deconv = torch.nn.ConvTranspose2d(in_channels=self.iFM, out_channels=self.oFM,
+                                               kernel_size=(self.kernel_time, self.kernel_freq),
+                                               stride=(self.stride_time, self.stride_freq),
+                                               padding=(self.pad_time, self.pad_freq),
+                                               dilation=(self.dilation_time, self.dilation_freq))
+
+    def forward(self, x):
+        x = self.deconv(x)
+        return x
--- model/DNN.py
+++ model/DNN.py
@@ -4,14 +4,103 @@
 # File Name:        DNN.py
 # Author:           Qingzheng WANG
 # Time:             2023/7/1 15:46
-# Description:                       
+# Description:      implementation of DNN1
 # Function List:    
 # ===================================================
+
 import torch
+from Blocks import *
+
+class DNN(torch.nn.Module):
+    def __init__(self, iFM, oFM, T, F, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.iFM = iFM # input Feature Maps
+        self.T = T # time steps
+        self.F = F # frequency bins
+        self.oFM = oFM # output Feature Maps
+
+        self.conv2d_cauln = Conv2DCauLN(iFM=self.iFM, oFM=30, T=self.T, F=self.F,
+                                        kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=1,
+                                        pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+
+        self.resblock = ResBlock(iFM=30, oFM=30, T=self.T, F=self.F)
+
+        self.conv2d_prelu_bn30 = Conv2DPReLUBN(iFM=30, oFM=30, T=self.T, F=self.F,
+                                              kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=2,
+                                              pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+        self.conv2d_prelu_bn64 = Conv2DPReLUBN(iFM=30, oFM=64, T=self.T, F=self.F,
+                                               kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=2,
+                                               pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+        self.conv2d_prelu_bn192 = Conv2DPReLUBN(iFM=64, oFM=192, T=self.T, F=self.F,
+                                                kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=1,
+                                                pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+
+        self.deconv2d_prelu_bn6430 = Deconv2DPReLUBN(iFM=64, oFM=30, T=self.T, F=self.F,
+                                                   kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=2,
+                                                   pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+        self.deconv2d_prelu_bn30 = Deconv2DPReLUBN(iFM=30, oFM=30, T=self.T, F=self.F,
+                                                   kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=2,
+                                                   pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+        self.deconv2d_prelu_bn64 = Deconv2DPReLUBN(iFM=192, oFM=64, T=self.T, F=self.F,
+                                                   kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=1,
+                                                   pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+
+        self.skip_connection30 = Conv2DPReLUBN(iFM=30, oFM=30, T=self.T, F=self.F,
+                                               kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=1,
+                                               pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+        self.skip_connection64 = Conv2DPReLUBN(iFM=64, oFM=64, T=self.T, F=self.F,
+                                               kernel_time=1, kernel_freq=1, stride_time=1, stride_freq=1,
+                                               pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+        self.skip_connection192 = Conv2DPReLUBN(iFM=192, oFM=192, T=self.T, F=self.F,
+                                                kernel_time=1, kernel_freq=1, stride_time=1, stride_freq=1,
+                                                pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+
+        self.deconv2d = Deconv2D(iFM=30, oFM=self.oFM, T=self.T, F=self.F,
+                                 kernel_time=1, kernel_freq=3, stride_time=1, stride_freq=1,
+                                 pad_time=0, pad_freq=0, dilation_time=1, dilation_freq=1)
+
+        self.lstm = torch.nn.LSTM(input_size=self.T, hidden_size=300, num_layers=3, batch_first=True, proj_size=self.T)
+
+
+    def forward(self, x):
+        s = []
+        x = self.conv2d_cauln(x)
+        x = self.resblock(x)
+        s.append(x)
+        for i in range(4):
+            x = self.conv2d_prelu_bn30(x)
+            x = self.resblock(x)
+            s.append(x)
+        x = self.conv2d_prelu_bn64(x)
+        s.append(x)
+        x = self.conv2d_prelu_bn192(x)
+        s.append(x)
+        x = x.reshape(x.shape[0], x.shape[1], -1) # (batch, channel, time*freq)
+        x, _ = self.lstm(x)
+        x = x.reshape(x.shape[0], x.shape[1], x.shape[2], 1)
+        x += s[-1]
+        x = self.deconv2d_prelu_bn64(x)
+        x += s[-2]
+        x = self.deconv2d_prelu_bn6430(x)
+        t = -2
+        for i in range(4):
+            t -= 1
+            x += s[t]
+            x = self.resblock(x)
+            x = self.deconv2d_prelu_bn30(x)
+        x += s[0]
+        x = self.resblock(x)
+        x = self.deconv2d(x)
+        return x
+
+
 
 
 if __name__ == '__main__':
-    pass
+    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+    ino = torch.randn(1, 1, 256, 129).to(device)
+    n = DNN(1, 1, 256, 127).to(device)
+    y = n(ino)
 
 
 
